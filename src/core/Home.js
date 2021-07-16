1bd6b52fbf16b48053a7e92f0696ad8531dcd5ce
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-07-16 16:01:46 +0700

    Section 11: React - Categories and Products

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-
+import { getProducts } from './apiCore';
+import Card from './Card';
 
 const Home = () => {
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsBySell(data);
+            }
+        });
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsByArrival(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
 
     return (
         <Layout
@@ -10,7 +41,23 @@ const Home = () => {
             description="Node React E-commerce App"
             className="container-fluid"
         >
-            Home
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card product={product} />
+                    </div>
+                ))}
+            </div>
+
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySell.map((product, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card product={product} />
+                    </div>
+                ))}
+            </div>
         </Layout>
     );
 };
