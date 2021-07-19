69f4bb2312744d7836f081219fdbb041b879d636
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-07-19 16:50:49 +0700

    Section 13: React - Products Search

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import { getProducts } from './apiCore';
 import Card from './Card';
+import Search from './Search';
 
 const Home = () => {
     const [productsBySell, setProductsBySell] = useState([]);
     const [productsByArrival, setProductsByArrival] = useState([]);
     const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
 
     const loadProductsBySell = () => {
         getProducts('sold').then(data => {
@@ -41,6 +41,7 @@ const Home = () => {
             description="Node React E-commerce App"
             className="container-fluid"
         >
+            <Search />
             <h2 className="mb-4">New Arrivals</h2>
             <div className="row">
                 {productsByArrival.map((product, i) => (
